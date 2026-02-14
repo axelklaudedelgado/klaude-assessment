fae193d0da5e8ae889c2bd85d081f63ad275aef5
axelklaudedelgado <144205486+axelklaudedelgado@users.noreply.github.com>
2026-02-14 21:44:18 +0800

    chore: scaffold ShopifyOAuthController and routes

---
+++ laravel-api/app/Http/Controllers/ShopifyOAuthController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class ShopifyOAuthController extends Controller
+{
+    public function install(Request $request)
+    {
+        //
+    }
+
+    public function callback(Request $request)
+    {
+        //
+    }
+}
--- laravel-api/routes/web.php
+++ laravel-api/routes/web.php
@@ -1,3 +1,11 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ShopifyOAuthController;
+
+// Shopify OAuth routes
+Route::get('/shopify/install', [ShopifyOAuthController::class, 'install'])
+    ->name('shopify.install');
+
+Route::get('/shopify/callback', [ShopifyOAuthController::class, 'callback'])
+    ->name('shopify.callback');
